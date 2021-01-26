2fe779545cfe6aae72d3c9997be97f9bd755f07e
kutakutat <18190905+kutakutat@users.noreply.github.com>
2021-01-26 11:16:57 +0900

    MySQL の導入

--- app/src/test/java/com/example/janken/AppTest.java
+++ app/src/test/java/com/example/janken/AppTest.java
@@ -1,10 +1,12 @@
 package com.example.janken;
 
-import com.example.janken.infrastructure.csvdao.JankenCsvDao;
-import com.example.janken.infrastructure.csvdao.JankenDetailCsvDao;
+import com.example.janken.domain.dao.JankenDao;
+import com.example.janken.domain.dao.JankenDetailDao;
 import com.example.janken.domain.model.Hand;
 import com.example.janken.domain.model.JankenDetail;
 import com.example.janken.domain.model.Result;
+import com.example.janken.infrastructure.mysqldao.JankenDetailMySQLDao;
+import com.example.janken.infrastructure.mysqldao.JankenMySQLDao;
 import lombok.val;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -22,17 +24,13 @@ class AppTest {
 
     private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-    private static final String DATA_DIR = System.getProperty("user.dir") + "/../data/";
-    private static final String JANKENS_CSV = DATA_DIR + "jankens.csv";
-    private static final String JANKEN_DETAILS_CSV = DATA_DIR + "janken_details.csv";
-
     private static InputStream defaultStdin = System.in;
     private static PrintStream defaultStdout = System.out;
     private static StandardInputSnatcher stdinSnatcher;
     private static StandardOutputSnatcher stdoutSnatcher;
 
-    private static JankenCsvDao jankenCsvDao = new JankenCsvDao();
-    private static JankenDetailCsvDao jankenDetailCsvDao = new JankenDetailCsvDao();
+    private static JankenDao jankenDao = new JankenMySQLDao();
+    private static JankenDetailDao jankenDetailDao = new JankenDetailMySQLDao();
 
     @BeforeAll
     static void setup() {
@@ -73,8 +71,8 @@ class AppTest {
         stdinSnatcher.inputLine(String.valueOf(player1HandValue));
         stdinSnatcher.inputLine(String.valueOf(player2HandValue));
 
-        val jankensCountBeforeTest = jankenCsvDao.count();
-        val jankenDetailsCountBeforeTest = jankenDetailCsvDao.count();
+        val jankensCountBeforeTest = jankenDao.count();
+        val jankenDetailsCountBeforeTest = jankenDetailDao.count();
 
         // 実行
 
@@ -101,13 +99,13 @@ class AppTest {
         assertEquals(expectedStdout, actualStdout, "標準出力の内容が想定通りであること");
 
         // じゃんけんデータの CSV の検証
-        assertEquals(jankensCountBeforeTest + 1, jankenCsvDao.count(), "じゃんけんが 1 件追加されたこと");
+        assertEquals(jankensCountBeforeTest + 1, jankenDao.count(), "じゃんけんが 1 件追加されたこと");
         val expectedJankenId = jankensCountBeforeTest + 1;
-        val savedJanken = jankenCsvDao.findById(jankensCountBeforeTest + 1);
+        val savedJanken = jankenDao.findById(jankensCountBeforeTest + 1);
         assertTrue(savedJanken.isPresent(), "じゃんけんが保存されていること");
 
         // じゃんけん明細データの CSV の検証
-        assertEquals(jankenDetailsCountBeforeTest + 2, jankenDetailCsvDao.count(),
+        assertEquals(jankenDetailsCountBeforeTest + 2, jankenDetailDao.count(),
                 "じゃんけん明細が 2 行件追加されたこと");
 
         val expectedJankenDetail1Id = jankenDetailsCountBeforeTest + 1;
@@ -117,7 +115,7 @@ class AppTest {
                 1L,
                 Hand.of(player1HandValue),
                 Result.of(player1ResultValue));
-        val savedJankenDetail1 = jankenDetailCsvDao.findById(expectedJankenDetail1Id);
+        val savedJankenDetail1 = jankenDetailDao.findById(expectedJankenDetail1Id);
         assertEquals(expectedJankenDetail1, savedJankenDetail1.get(),
                 "じゃんけん明細に追加された 1 件目の内容が想定通りであること");
 
@@ -128,7 +126,7 @@ class AppTest {
                 2L,
                 Hand.of(player2HandValue),
                 Result.of(player2ResultValue));
-        val savedJankenDetail2 = jankenDetailCsvDao.findById(expectedJankenDetail2Id);
+        val savedJankenDetail2 = jankenDetailDao.findById(expectedJankenDetail2Id);
         assertEquals(expectedJankenDetail2, savedJankenDetail2.get(),
                 "じゃんけん明細に追加された 2 件目の内容が想定通りであること");
     }
@@ -176,6 +174,7 @@ class AppTest {
 
         assertEquals(expected, actual);
     }
+
 }
 
 class StandardInputSnatcher extends InputStream {
